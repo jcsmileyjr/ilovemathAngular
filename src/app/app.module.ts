c852f71c9ef5f35dab6cc74066c761cd472ed3b5
JC Smiley <jcsmileyjr@gmail.com>
2019-01-19 13:21:05 -0600

    Created Header Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AppTitleComponent } from './app-title/app-title.component';
+import { HeaderComponent } from './header/header.component';
+import { EquationComponent } from './equation/equation.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AppTitleComponent
+    AppTitleComponent,
+    HeaderComponent,
+    EquationComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
