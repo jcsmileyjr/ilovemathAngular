8f0ead83772c6bfe28f201cb514631d46361d2e7
JC Smiley <jcsmileyjr@gmail.com>
2019-01-23 21:35:37 -0600

    Added style, new game button, etc. to final page

--- src/app/final/final.component.ts
+++ src/app/final/final.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {Observable} from 'rxjs';
+import { Router } from '@angular/router'; //needed for routing imperatively in your components. You must also add code to the constructor. 
+
+import {ScoreService} from '../score.service';
 
 @Component({
   selector: 'app-final',
@@ -7,9 +11,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FinalComponent implements OnInit {
 
-  constructor() { }
+  userScore = 0; // it shows the current score.  
+
+  constructor(public score: ScoreService, private router: Router) { }
 
   ngOnInit() {
+      //This method async update the score variable with the Score servie currentScore variable
+      this.score.currentScore.subscribe((value) => {
+          this.userScore = value;
+      });      
+  }
+
+  newGame(){
+      this.router.navigateByUrl('/home');
   }
 
 }
