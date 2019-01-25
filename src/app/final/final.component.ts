1c8291553607e5ba07a3a0c54d8ba0dae50349e0
JC Smiley <jcsmileyjr@gmail.com>
2019-01-25 16:45:09 -0600

    Initial use of Icon Flag on Final page

--- src/app/final/final.component.ts
+++ src/app/final/final.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Observable} from 'rxjs';
 import { Router } from '@angular/router'; //needed for routing imperatively in your components. You must also add code to the constructor. 
+import { faFlagCheckered } from '@fortawesome/free-solid-svg-icons';
 
 import {ScoreService} from '../score.service';
 
@@ -12,6 +13,8 @@ import {ScoreService} from '../score.service';
 export class FinalComponent implements OnInit {
 
   userScore = 0; // it shows the current score.  
+
+  faFlagCheckered = faFlagCheckered;
 
   constructor(public score: ScoreService, private router: Router) { }
 
@@ -27,3 +30,9 @@ export class FinalComponent implements OnInit {
   }
 
 }
+
+//The idea is for a icon runner to run across the screen to a black and white checkered flag (far right side of screen).
+//It would be great for a animated dust cloud to be behind the player or something to show movement.
+//If the player scores high, then show blue flag with gold trophy in middle.
+//if the player scores low, show red flags
+//On the home page allow the playter to type in his name and favorite color. The runner icon can be that color and the player name used throughout app.
