d8e709551022fa4dfa813dee65cb876a01d4d0a2
JC Smiley <jcsmileyjr@gmail.com>
2019-01-29 09:34:24 -0600

    Update the calculate service
    - Refactor to use a random number (1-5) a the bottom line of the equation. Except  in the division test its stll 1.

--- src/app/calculate.service.ts
+++ src/app/calculate.service.ts
@@ -8,12 +8,13 @@ export class CalculateService {
   currentOperatorTitle;//initate a varible that will hold the current page type to be use for calculations
   currentOperator; //initate a varible that hold current operator type. Updated when the page is loaded and in the updateOperatorTitle()
   currentTopLine = 0; //initate equation top line variable
+  currentBottomLine = 0; //initate equation bottom line variable
   current = 0; //initate temp variable used to check if the currentTopLine is the previous currentTopLine
   correctAnswer = 0;  //initate varible for correct answer to current equation  
 
   constructor() { }
     
-  //call in the equation component when the user press the submit button or press the enter key. This also check to make sure the same number isn't chosen twice in a row
+  //called in the Equation component when the user press the submit button or press the enter key. This also check to make sure the same number isn't chosen twice in a row
   getRandomTopLineNumber(){
       this.current = this.currentTopLine;
       this.currentTopLine = Math.floor(Math.random()*11);
@@ -21,6 +22,16 @@ export class CalculateService {
           this.currentTopLine = Math.floor(Math.random()*11);
       }
       return this.currentTopLine;
+  }
+
+  //called in the Equation component when the user press the submit button or press the enter key. This also check to make sure the same number isn't chosen twice in a row
+  getRandomBottomLineNumber(){
+      this.current = this.currentBottomLine;
+      this.currentBottomLine = Math.floor(Math.random()*6);
+      if(this.current == this.currentBottomLine){
+          this.currentBottomLine = Math.floor(Math.random()*6);
+      }
+      return this.currentBottomLine;
   }
 
   //method called inside each page onInit to update the page title on the header component
@@ -51,21 +62,21 @@ export class CalculateService {
   //Used the type of operator to decide which equation to use
   checkUserAnswer(userAnwer){
       if(this.currentOperatorTitle == "Addition"){
-          this.correctAnswer = this.currentTopLine + 1;
+          this.correctAnswer = this.currentTopLine + this.currentBottomLine;
           if(this.correctAnswer == userAnwer){
               return true;
           }else{
               return false;
           }
       }else if(this.currentOperatorTitle =="Subtraction"){
-          this.correctAnswer = this.currentTopLine - 1;
+          this.correctAnswer = this.currentTopLine - this.currentBottomLine;
           if(this.correctAnswer == userAnwer){
               return true;
           }else{
               return false;
           }
       }else if(this.currentOperatorTitle =="Multiplication"){
-          this.correctAnswer = this.currentTopLine * 1;
+          this.correctAnswer = this.currentTopLine * this.currentBottomLine;
           if(this.correctAnswer == userAnwer){
               return true;
           }else{
